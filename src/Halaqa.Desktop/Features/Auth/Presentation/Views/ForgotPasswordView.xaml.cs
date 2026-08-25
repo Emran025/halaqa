74966d6f86c9b96837568467a04ee191e2f7b006
Emran Nasser <148263577+Emran025@users.noreply.github.com>
2026-08-25 11:11:08 +0300

    feat: add password recovery presentation (#4)
    Co-authored-by: Emran025 <1.48263577e+08+Emran025@users.noreply.github.com>

---
+++ src/Halaqa.Desktop/Features/Auth/Presentation/Views/ForgotPasswordView.xaml.cs
@@ -0,0 +1,11 @@
+using System.Windows.Controls;
+
+namespace Halaqa.Desktop.Features.Auth.Presentation.Views;
+
+public partial class ForgotPasswordView : UserControl
+{
+    public ForgotPasswordView()
+    {
+        InitializeComponent();
+    }
+}
